90ef4bac2660582ef4a8e145b89e2573fac56b20
KenspireTech <40768960+KenspireTech@users.noreply.github.com>
2020-03-28 11:27:20 +0530

    Update const.js

--- web-basic-sample/src/js/const.js
+++ web-basic-sample/src/js/const.js
@@ -1,4 +1,4 @@
-export const APP_ID = '9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23';
+export const APP_ID = '3EA4C8F3-99E7-476D-87E7-2E5F6798285A';
 export const USER_ID = 'user_id';
 export const DISPLAY_NONE = 'none';
 export const DISPLAY_BLOCK = 'block';
